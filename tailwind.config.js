cbae877968d6ae4592f9eb53d0161e9ccb41cf73
Fabrício Maxwell <102274424+FabricioMaxwell@users.noreply.github.com>
2023-04-26 07:30:12 -0300

    Update Summary

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,10 +16,9 @@ module.exports = {
         'Light-slate-blue-background': 'hsl(252, 100%, 67%)',
         'Light-royal-blue-background': 'hsl(241, 81%, 54%)',
         'Violet-blue-circle': 'hsla(256, 72%, 46%, 1)',
-        'Persian-blue-circle': 'hsla(241, 72%, 46%, 1)',
-        'Persian-blue-circle01': 'hsla(241, 72%, 46%, 0)',
+        'Persian-blue-circle': 'hsla(241, 72%, 46%, 0)',
 
-        'Pale-blue': 'hsl(221, 100%, 80%)',
+        'Pale-blue': 'hsl(221, 100%, 80 %)',
         'Light-lavender': 'hsl(241, 100%, 89%)',
         'Dark-gray-blue': 'hsl(224, 30%, 27%)'
       },
